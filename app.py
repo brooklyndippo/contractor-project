c87e8486e9ea5a4fd1c1c7c3fd7d7ccbadca9316
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-11-15 13:02:33 -1000

    added mongodb and donation model

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Donations
+donations = db.donations
 
 app = Flask(__name__)
 
 
 # Mock donations
-donations = [
-    {'organization': 'Save the turtles', 'amount_usd': 400, 'donor': 'Joe Schmo'},
-    {'organization': 'Save the seagulls', 'amount_usd': 100, 'donor': 'Terry Hairy'}
-]
+# donations = [
+#     {'organization': 'Save the turtles', 'amount_usd': 400, 'donor': 'Joe Schmo'},
+#     {'organization': 'Save the seagulls', 'amount_usd': 100, 'donor': 'Terry Hairy'}
+# ]
 
 # READ - http verb GET - see donations and forms for changes
     #INDEX - show all of the donations
 @app.route('/')
 def donations_index():
     '''Show all donations.'''
-    return render_template('donations_index.html', donations=donations)
+    return render_template('donations_index.html', donations=donations.find())
 
 if __name__ == '__main__':
     app.run(debug=True)
