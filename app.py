3d9a569b95d4973aeba6db9e1d977cf02e00438e
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-11-15 09:39:07 -1000

    outlined 7 resourceful routes

--- app.py
+++ app.py
@@ -9,3 +9,14 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+# Create an app that can track donations and impact for a user
+# CREATE - http verb POST - create a new donation
+# READ - http verb GET - see donations and forms for changes
+    #INDEX - show all of the donations
+    #SHOW - show a single donation
+    #NEW - form to create a new donation
+    #EDIT - form to update an existing donation
+# UPDATE - http verb PUT PATCH - update a donation
+# DESTROY - http verb DELETE - delete a donation
