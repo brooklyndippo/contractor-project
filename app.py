d63d57d3217200db3d4827616d40a1b454db007d
Brooklyn Dippo <brooklyndippo@gmail.com>
2021-11-15 14:33:16 -1000

    users can edit and update a donation

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 client = MongoClient()
 db = client.Donations
@@ -26,8 +27,11 @@ def donations_index():
 @app.route('/donations/new')
 def donations_new():
     '''Create a new donation.'''
-    return render_template('donations_new.html')
+    donation = {}
+    return render_template('donations_new.html', donation=donation, title='New Donation')
 
+
+# CREATE - http verb POST - create a new donation
 @app.route('/donations', methods=['POST'])
 def donations_submit(): 
     '''Submit a new donation.'''
@@ -39,16 +43,52 @@ def donations_submit():
     donations.insert_one(donation)
     return redirect(url_for('donations_index'))
 
+
+# READ - http verb GET - see donations and forms for changes
+    # SHOW - show a single donation
+@app.route('/donations/<donation_id>')
+def donations_show(donation_id):
+    '''Show a single donation'''
+    donation = donations.find_one({'_id': ObjectId(donation_id)})
+    return render_template('donations_show.html', donation=donation)
+
+
+
+# READ - http verb GET - see donations and forms for changes
+    #EDIT - form to update an existing donation
+@app.route('/donations/<donation_id>/edit')
+def donations_edit(donation_id):
+    '''Show the edit form for a donation'''
+    donation = donations.find_one({'_id': ObjectId(donation_id)})
+    return render_template('donations_edit.html', donation=donation, title='Edit Donation')
+
+
+
+#UPDATE A Donation
+@app.route('/donations/<donation_id>', methods=['POST'])
+def donations_update(donation_id):
+    """Submit an updated donation."""
+    # create our updated donation
+    updated_donation = {
+        'organization': request.form.get('organization'),
+        'amount_usd': request.form.get('amount_usd'),
+        'note': request.form.get('note')
+    }
+    # set the former donation to the new one we just updated/edited
+    donations.update_one(
+        {'_id': ObjectId(donation_id)},
+        {'$set': updated_donation})
+    # take us back to the donation's show page
+    return redirect(url_for('donations_show', donation_id=donation_id))
+
 if __name__ == '__main__':
     app.run(debug=True)
 
 
 # Create an app that can track donations and impact for a user
-# CREATE - http verb POST - create a new donation
+
 # READ - http verb GET - see donations and forms for changes
-    #INDEX - show all of the donations
-    #SHOW - show a single donation
-    #NEW - form to create a new donation
+
     #EDIT - form to update an existing donation
 # UPDATE - http verb PUT PATCH - update a donation
 # DESTROY - http verb DELETE - delete a donation
